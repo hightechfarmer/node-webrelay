80da6670527531389ac0c7a9a6dbcf9a8db74238
Nicholas Kristoffersen <nicholas.kristoffersen@gmail.com>
2017-02-18 22:39:10 -0800

    adding Q for the first time

--- README.md
+++ README.md
@@ -13,19 +13,31 @@ const webRelay = require('@nkristoffersen/node-webrelay')('http://192.168.1.2');
 ```
 - Get relay status. The callback is the status returned from the web relay in JSON format.
 ```
-webRelay.status(function (data) {
-	console.log('data', data);
-});
+webRelay.status()
+  .then(function (result) {
+    console.log(result);
+  })
+  .fail(function (error) {
+    console.log(error);
+  });
 ```
 - Close relay (on). The callback is the state returned from the web relay in JSON format.
 ```
-webRelay.close(function (data) {
-	console.log('data', data);
-});
+webRelay.close()
+  .then(function (result) {
+    console.log(result);
+  })
+  .fail(function (error) {
+    console.log(error);
+  });
 ```
 - Open relay (off). The callback is the state returned from the web relay in JSON format.
 ```
-webRelay.open(function (data) {
-	console.log('data', data);
-});
+webRelay.open()
+  .then(function (result) {
+    console.log(result);
+  })
+  .fail(function (error) {
+    console.log(error);
+  });
 ```
--- index.js
+++ index.js
@@ -1,48 +1,54 @@
 'use strict';
 var request = require('request');
 var parser = require('xml2json');
+var Q = require('q');
 
 module.exports = function (relayAddress) {
 	var module = {};
 
 	module.status = function (callback) {
+		var deferred = Q.defer();
 		request.get(relayAddress + '/stateFull.xml', function (error, response, body) {
 			if (!error && response.statusCode == 200) {
 				var json = JSON.parse(parser.toJson(body));
 				var relayResponse = json.datavalues
-				callback(relayResponse);
+				deferred.resolve(relayResponse);
 			} else {
-				callback('error!');
+				deferred.reject('error with getting relay status!');
 			}
 		});
+		deferred.promise.nodeify(callback);
+		return deferred.promise;
 	};
 
 	module.close = function (callback) {
+		var deferred = Q.defer();
 		request.get(relayAddress + '/stateFull.xml?relayState=1', function (error, response, body) {
 			if (!error && response.statusCode == 200) {
 				var json = JSON.parse(parser.toJson(body));
-				var relayResponse = {
-					relayState: json.datavalues.relaystate
-				};
-				callback(relayResponse);
+				var relayResponse = json.datavalues
+				deferred.resolve(relayResponse);
 			} else {
-				callback('error!');
+				deferred.reject('error with setting relay to 1!');
 			}
 		});
+		deferred.promise.nodeify(callback);
+		return deferred.promise;
 	};
 
 	module.open = function (callback) {
+		var deferred = Q.defer();
 		request.get(relayAddress + '/stateFull.xml?relayState=0', function (error, response, body) {
 			if (!error && response.statusCode == 200) {
 				var json = JSON.parse(parser.toJson(body));
-				var relayResponse = {
-					relayState: json.datavalues.relaystate
-				};
-				callback(relayResponse);
+				var relayResponse = json.datavalues
+				deferred.resolve(relayResponse);
 			} else {
-				callback('error!');
+				deferred.reject('error with setting relay to 0!');
 			}
 		});
+		deferred.promise.nodeify(callback);
+		return deferred.promise;
 	};
 
 	return module;
