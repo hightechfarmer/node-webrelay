ea0f88d52e6f7167c98104c83cbae1312cc6ea53
Nicholas Kristoffersen <nick@kristoffersen.io>
2017-05-24 12:12:46 +0200

    passing relay ip address to each function individually instead of globally

--- README.md
+++ README.md
@@ -9,11 +9,11 @@ npm install nkristoffersen/node-webrelay
 ## usage
 - Add to your node project and include the local IP address of the web relay.
 ```javascript
-const webRelay = require('@nkristoffersen/node-webrelay')('http://192.168.1.2');
+const webRelay = require('@nkristoffersen/node-webrelay').webRelay;
 ```
 - Get relay status. The callback is the status returned from the web relay in JSON format.
 ```javascript
-webRelay.status()
+webRelay.status(relayIpAddress)
   .then(function (result) {
     console.log(result);
   })
@@ -23,7 +23,7 @@ webRelay.status()
 ```
 - Close relay (on). The callback is the state returned from the web relay in JSON format.
 ```javascript
-webRelay.close()
+webRelay.close(relayIpAddress)
   .then(function (result) {
     console.log(result);
   })
@@ -33,7 +33,7 @@ webRelay.close()
 ```
 - Open relay (off). The callback is the state returned from the web relay in JSON format.
 ```javascript
-webRelay.open()
+webRelay.open(relayIpAddress)
   .then(function (result) {
     console.log(result);
   })
--- index.js
+++ index.js
@@ -3,53 +3,51 @@ var request = require('request');
 var parser = require('xml2json');
 var Q = require('q');
 
-module.exports = function (relayAddress) {
-	var module = {};
+var webRelay = function () {};
 
-	module.status = function (callback) {
-		var deferred = Q.defer();
-		request.get(relayAddress + '/stateFull.xml', function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				var json = JSON.parse(parser.toJson(body));
-				var relayResponse = json.datavalues
-				deferred.resolve(relayResponse);
-			} else {
-				deferred.reject('error with getting relay status!');
-			}
-		});
-		deferred.promise.nodeify(callback);
-		return deferred.promise;
-	};
-
-	module.close = function (callback) {
-		var deferred = Q.defer();
-		request.get(relayAddress + '/stateFull.xml?relayState=1', function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				var json = JSON.parse(parser.toJson(body));
-				var relayResponse = json.datavalues
-				deferred.resolve(relayResponse);
-			} else {
-				deferred.reject('error with setting relay to 1!');
-			}
-		});
-		deferred.promise.nodeify(callback);
-		return deferred.promise;
-	};
-
-	module.open = function (callback) {
-		var deferred = Q.defer();
-		request.get(relayAddress + '/stateFull.xml?relayState=0', function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				var json = JSON.parse(parser.toJson(body));
-				var relayResponse = json.datavalues
-				deferred.resolve(relayResponse);
-			} else {
-				deferred.reject('error with setting relay to 0!');
-			}
-		});
-		deferred.promise.nodeify(callback);
-		return deferred.promise;
-	};
-
-	return module;
+webRelay.prototype.status = function (relayAddress, callback) {
+	var deferred = Q.defer();
+	request.get(relayAddress + '/stateFull.xml', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var json = JSON.parse(parser.toJson(body));
+			var relayResponse = json.datavalues
+			deferred.resolve(relayResponse);
+		} else {
+			deferred.reject('error with getting relay status!');
+		}
+	});
+	deferred.promise.nodeify(callback);
+	return deferred.promise;
 };
+
+webRelay.prototype.close = function (relayAddress, callback) {
+	var deferred = Q.defer();
+	request.get(relayAddress + '/stateFull.xml?relayState=1', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var json = JSON.parse(parser.toJson(body));
+			var relayResponse = json.datavalues
+			deferred.resolve(relayResponse);
+		} else {
+			deferred.reject('error with setting relay to 1!');
+		}
+	});
+	deferred.promise.nodeify(callback);
+	return deferred.promise;
+};
+
+webRelay.prototype.open = function (relayAddress, callback) {
+	var deferred = Q.defer();
+	request.get(relayAddress + '/stateFull.xml?relayState=0', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var json = JSON.parse(parser.toJson(body));
+			var relayResponse = json.datavalues
+			deferred.resolve(relayResponse);
+		} else {
+			deferred.reject('error with setting relay to 0!');
+		}
+	});
+	deferred.promise.nodeify(callback);
+	return deferred.promise;
+};
+
+exports.webRelay = new webRelay();
