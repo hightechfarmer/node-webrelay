cfd6e2fa435a27265167371880f42192113fb49a
Kevin Christiansen <kevin.balouch@gmail.com>
2018-03-01 16:12:04 +0100

    More fixes

--- index.js
+++ index.js
@@ -2,23 +2,18 @@
 const axios = require('axios')
 
 function WebRelay (address) {
-  let request
 
   if (!address || typeof address !== 'string') {
     throw {
       err: '[WebRelay]: Address cannot be blank or a non-string value.'
     }
-  } else {
-    request = axios.create({
-      baseURL: address
-    })
   }
 
   const API = {
     status () {
       // TODO: implement
       return new Promise((resolve, reject) => {
-        request.get('/stateFull.xml').then(res => {
+        axios.get(address + '/stateFull.xml').then(res => {
           if (res.data) {
             resolve()
           } else {
@@ -29,7 +24,7 @@ function WebRelay (address) {
     },
     open () {
       return new Promise((resolve, reject) => {
-        request.get('/stateFull.xml?relayState=0').then(res => {
+        axios.get(address + '/stateFull.xml?relayState=0').then(res => {
           if (res.status === 200) {
             resolve()
           } else {
@@ -40,7 +35,7 @@ function WebRelay (address) {
     },
     close () {
       return new Promise((resolve, reject) => {
-        request.get('/stateFull.xml?relayState=1').then(res => {
+        axios.get(address + '/stateFull.xml?relayState=1').then(res => {
           if (res.status === 200) {
             resolve()
           } else {
@@ -53,5 +48,5 @@ function WebRelay (address) {
 
   return API
 }
-
+q
 exports.webRelay = WebRelay
